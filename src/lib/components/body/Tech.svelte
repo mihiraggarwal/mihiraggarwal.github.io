e52223ba6943db25b71ec8921c1b973ae8895918
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2023-01-31 23:11:00 +0530

    tech projects

--- src/lib/components/body/Tech.svelte
+++ src/lib/components/body/Tech.svelte
@@ -1,8 +1,28 @@
-<p>tech</p>
+<script>
+    let repos = ["mihiraggarwal/select-gpt", "mihiraggarwal/realtio-npm", "mihiraggarwal/rollertoaster", "mihiraggarwal/spotify-permix", "mihiraggarwal/assist-co", "techsyndicate/encryptid-21", "mihiraggarwal/toi-notif", "mihiraggarwal/klear-app", "mihiraggarwal/quiz-platform", "techsyndicate/exun-21", "mihiraggarwal/riviore", "mihiraggarwal/tablot", "mihiraggarwal/mihiraggarwal.github.io", "mihiraggarwal/mockinv"]
+</script>
+
+<div class="repos">
+    {#each repos as repo}
+        <a href="https://github.com/{repo}" target="_blank" rel="noreferrer">
+            <img src="https://socialify.git.ci/{repo}/image?description=1&language=1&name=1&owner=1&pattern=Solid&stargazers=1&theme=Dark" alt={repo} />
+        </a>
+    {/each}
+</div>
 
 <style>
-    p {
-        font-size: 1.5rem;
-        font-family: airbnb-medium;
+    .repos {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: flex-start;
+        flex-wrap: wrap;
+    }
+
+    img {
+        max-width: 530px;
+        margin-bottom: 2vh;
+        border-radius: 5px;
+        opacity: 0.85;
     }
 </style>
