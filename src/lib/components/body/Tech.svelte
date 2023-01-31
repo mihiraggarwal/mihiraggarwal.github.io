8685faf591722cd7d91a2fb6a380a3a6954651da
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2023-02-01 02:10:47 +0530

    tech filters

--- src/lib/components/body/Tech.svelte
+++ src/lib/components/body/Tech.svelte
@@ -1,16 +1,46 @@
 <script>
-    let repos = ["mihiraggarwal/select-gpt", "mihiraggarwal/realtio-npm", "mihiraggarwal/rollertoaster", "mihiraggarwal/spotify-permix", "mihiraggarwal/assist-co", "techsyndicate/encryptid-21", "mihiraggarwal/toi-notif", "mihiraggarwal/klear-app", "mihiraggarwal/quiz-platform", "techsyndicate/exun-21", "mihiraggarwal/riviore", "mihiraggarwal/tablot", "mihiraggarwal/mihiraggarwal.github.io", "mihiraggarwal/mockinv"]
+    import Language from "$lib/components/Language.svelte";
+    import { current } from "$lib/components/body/stores"
+    
+    let languages = ["Node.js", "Python", "Android", "Vanilla.js", "Svelte"]
+
+    let repos = {"mihiraggarwal/select-gpt": "Vanilla.js", "mihiraggarwal/realtio-npm": "Vanilla.js", "mihiraggarwal/rollertoaster": "Node.js", "mihiraggarwal/spotify-permix": "Python", "mihiraggarwal/assist-co": "Android", "techsyndicate/encryptid-21": "Node.js", "mihiraggarwal/toi-notif": "Python", "mihiraggarwal/klear-app": "Android", "mihiraggarwal/quiz-platform": "Node.js", "techsyndicate/exun-21": "Node.js", "mihiraggarwal/riviore": "Android", "mihiraggarwal/tablot": "Python", "mihiraggarwal/mihiraggarwal.github.io": "Svelte", "mihiraggarwal/mockinv": "Python"}
+
+    $: clang = $current
 </script>
 
-<div class="repos">
-    {#each repos as repo}
-        <a href="https://github.com/{repo}" target="_blank" rel="noreferrer">
-            <img src="https://socialify.git.ci/{repo}/image?description=1&language=1&name=1&owner=1&pattern=Solid&stargazers=1&theme=Dark" alt={repo} />
-        </a>
+<div class="language-container">
+    {#each languages as language}
+        <Language {language} />
     {/each}
 </div>
 
+<div class="repos">
+    {#if !clang}
+        {#each Object.entries(repos) as [repo, language]}
+            <a href="https://github.com/{repo}" target="_blank" rel="noreferrer">
+                <img src="https://socialify.git.ci/{repo}/image?description=1&language=1&name=1&owner=1&pattern=Solid&stargazers=1&theme=Dark" alt={repo} />
+            </a>
+        {/each}
+    {:else}
+        {#each Object.entries(repos) as [repo, language]}
+            {#if language === clang}
+                <a href="https://github.com/{repo}" target="_blank" rel="noreferrer">
+                    <img src="https://socialify.git.ci/{repo}/image?description=1&language=1&name=1&owner=1&pattern=Solid&stargazers=1&theme=Dark" alt={repo} />
+                </a>
+            {/if}
+        {/each}
+    {/if}
+</div>
+
 <style>
+    .language-container {
+        display: flex;
+        margin-bottom: 2vh;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
     .repos {
         display: flex;
         flex-direction: row;
