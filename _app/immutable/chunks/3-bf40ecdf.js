5153f4af72eb561d1e72bb4294ca0e046f207389
mihiraggarwal <mihir.aggarwal1000@gmail.com>
2024-01-21 20:26:40 +0530

    Updates

---
+++ _app/immutable/chunks/3-bf40ecdf.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-fca24c55.js";import{default as t}from"../components/pages/music/_page.svelte-b868cfe9.js";export{t as component,r as universal};
--- _app/immutable/chunks/4-9f969f42.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/tech/_page.svelte-f32deb06.js";export{t as component};
