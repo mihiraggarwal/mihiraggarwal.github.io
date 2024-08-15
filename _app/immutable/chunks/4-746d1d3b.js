8942d83ce4257ded7b8381a638675cda7589065b
mihiraggarwal <mihir.aggarwal1000@gmail.com>
2024-08-15 18:47:43 +0530

    Updates

---
+++ _app/immutable/chunks/4-746d1d3b.js
@@ -0,0 +1,1 @@
+const e=!0;export{e as has_server_load};
---
+++ _app/immutable/chunks/5-ef6a0f42.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-da46b06b.js";import{default as t}from"../components/pages/tech/_page.svelte-ccf0a8ee.js";export{t as component,r as universal};
